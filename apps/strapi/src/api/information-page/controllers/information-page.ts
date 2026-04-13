a54096553d38ea7e2786ab74f404a9f29f0e73a7
Aleksander Grzybek <ajemalbatros@gmail.com>
2026-04-13 20:44:14 +0200

    feat: basic strapi api structure

---
+++ apps/strapi/src/api/information-page/controllers/information-page.ts
@@ -0,0 +1,7 @@
+/**
+ * information-page controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::information-page.information-page');
---
+++ apps/strapi/src/api/information-page/routes/information-page.ts
@@ -0,0 +1,7 @@
+/**
+ * information-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::information-page.information-page');
