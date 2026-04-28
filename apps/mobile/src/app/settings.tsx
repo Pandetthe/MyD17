499d3c0c7c8c0aec8373afee0cbb591dcff3bcf1
dszlapa <dawidszlapaz@gmail.com>
2026-04-28 18:27:56 +0200

    Add: Settings for settings page

--- apps/mobile/src/app/settings.tsx
+++ apps/mobile/src/app/settings.tsx
@@ -1,20 +1,33 @@
 import { View } from "react-native";
 import {StyleSheet} from "react-native-unistyles";
 import {Theme} from "@/styles/themes/theme";
-import {Home} from "lucide-react-native";
+import {BellIcon, BellRingIcon, InfoIcon, LanguagesIcon, MoonIcon} from "lucide-react-native";
 import {useState} from "react";
 import Setting from "@/components/SwitchSetting";
 
 export default function Settings() {
-    const [switchValue, setSwitchValue] = useState(0);
-    const buttonClick = () => {
-        setSwitchValue(1 - switchValue);
+    const [darkMode, setDarkMode] = useState(0);
+    const [notifications, setNotifications] = useState(0);
+
+    const darkModeClick = () => {
+        setDarkMode(1 - darkMode);
+    }
+    const notificationsClick = () => {
+        setNotifications(1 - notifications);
+    }
+    const openLanguage = () => {
+        alert("Languages") // Placeholder
+    }
+    const openSubscriptions = () => {
+        alert("Subscriptions") // Placeholder
     }
 
     return (
         <View style={styles.container}>
-            <Setting icon={Home} text={"Hello world"} onPress={buttonClick} value={switchValue}/>
-            <Setting icon={Home} text={"<a>Hello world</a>"} onPress={buttonClick}/>
+            <Setting icon={MoonIcon} text={"Dark Mode"} onPress={darkModeClick} value={darkMode}/>
+            <Setting icon={notifications === 1 ? BellRingIcon : BellIcon} text={"Enable notifications"} onPress={notificationsClick} value={notifications}/>
+            <Setting icon={LanguagesIcon} text={"Language"} onPress={openLanguage}/>
+            <Setting icon={InfoIcon} text={"Manage subscriptions"} onPress={openSubscriptions}/>
         </View>
     )
 }
