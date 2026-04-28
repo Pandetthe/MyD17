150a2fb00b0da44556eedafe23b5f51597d6168c
dszlapa <dawidszlapaz@gmail.com>
2026-04-28 20:16:27 +0200

    Add: Settings in Drawer

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -2,11 +2,23 @@ import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import React from "react";
 import { Drawer } from "expo-router/drawer";
 import "@/styles/unistyles";
+import Icon from "@/components/core/Icon.component";
+import {SettingsIcon} from "lucide-react-native";
 
 export default function Layout() {
   return (
     <ThemeProvider value={DefaultTheme}>
-      <Drawer />
+      <Drawer>
+        <Drawer.Screen
+            name="settings"
+            options={{
+              drawerLabel: "Ustawienia",
+              drawerIcon: () => (
+                  <Icon icon={SettingsIcon} hasBackground={false}/>
+              ),
+            }}
+        />
+      </Drawer>
     </ThemeProvider>
   );
 }
