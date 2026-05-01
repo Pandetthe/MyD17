050c81ab7fcd2f2d482a0e267c31018dc11108cd
stawkey <stanislawstrojniak@gmail.com>
2026-05-01 22:53:15 +0200

    feat: add Jest configuration and a test test

---
+++ apps/mobile/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: "jest-expo",
+  testEnvironment: "node",
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|react-native-reanimated|react-native-unistyles|react-native-gesture-handler|react-native-screens|expo-symbols|lucide-react-native|@react-navigation|expo-router|expo-linking|expo-font|expo-splash-screen|expo-status-bar|expo-constants|expo-web-browser|expo-system-ui|expo-device|expo-image|react-native-drawer-layout)/",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@/assets/(.*)$": "<rootDir>/assets/$1",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/app/**"],
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        jsx: "react-jsx",
+      },
+    },
+  },
+};
---
+++ apps/mobile/src/tests/test.test.tsx
@@ -0,0 +1,5 @@
+import Button from "@/components/core/Button.component";
+
+test("Button is defined", () => {
+  expect(Button).toBeDefined();
+});
