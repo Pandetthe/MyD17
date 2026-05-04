8febd8490d8f7fd7f3216941f2713e30057031d1
stawkey <stanislawstrojniak@gmail.com>
2026-05-04 20:42:05 +0200

    fix: shut up eslint in jest.setup.js :D

--- apps/mobile/jest.setup.js
+++ apps/mobile/jest.setup.js
@@ -1,12 +1,9 @@
-/* global jest */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* global jest, require */
 import "@testing-library/jest-native/extend-expect";
 
-jest.mock("react-native-reanimated", () =>
-  require("react-native-reanimated/mock"),
-);
-jest.mock("react-native-worklets", () =>
-  require("react-native-worklets/lib/module/mock"),
-);
+jest.mock("react-native-reanimated", () => require("react-native-reanimated/mock"));
+jest.mock("react-native-worklets", () => require("react-native-worklets/lib/module/mock"));
 jest.mock("react-native-unistyles", () => ({
   useUnistyles: () => ({
     theme: {},
