13b8e3906043c1f12f29b1d8bea6d425714b561a
AleGrz <ajemalbatros@gmail.com>
2026-05-04 19:51:02 +0200

    Revert "review changes"
    This reverts commit 2307731513fe0b2626ac757932e3e8d7804ee3eb.

--- apps/mobile/src/components/core/Tag.component.tsx
+++ apps/mobile/src/components/core/Tag.component.tsx
@@ -1,6 +1,11 @@
+import { useMemo } from "react";
 import { Text, View } from "react-native";
 import { ColorPalette } from "@/styles/themes/theme";
+<<<<<<< HEAD
 import { StyleSheet, useUnistyles } from "react-native-unistyles";
+=======
+import { useUnistyles } from "react-native-unistyles";
+>>>>>>> parent of 2307731 (review changes)
 
 type TagProps = {
   text: string;
@@ -10,13 +15,19 @@ type TagProps = {
 export default function Tag({ text, color = "primary" }: TagProps) {
   const { theme } = useUnistyles();
 
+<<<<<<< HEAD
   const styles = StyleSheet.create({
     container: {
+=======
+  const containerStyle = useMemo(
+    () => ({
+>>>>>>> parent of 2307731 (review changes)
       height: theme.size.lg,
       borderRadius: theme.borderRadius.full,
       backgroundColor: theme.colors[color].background.accent,
       borderColor: theme.colors[color].main,
       borderWidth: 1,
+<<<<<<< HEAD
       alignItems: "center",
       justifyContent: "center",
       paddingHorizontal: theme.spacing.sm,
@@ -30,6 +41,26 @@ export default function Tag({ text, color = "primary" }: TagProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{text}</Text>
+=======
+      alignItems: "center" as const,
+      justifyContent: "center" as const,
+      paddingHorizontal: theme.spacing.sm,
+    }),
+    [theme.colors[color], theme.borderRadius.full, theme.size.sm, theme.spacing.xs],
+  );
+
+  const textStyle = useMemo(
+    () => ({
+        color: theme.colors[color].main,
+        fontSize: theme.size.sm,
+    }),
+    [theme.colors[color].main],
+  )
+
+  return (
+    <View style={containerStyle}>
+      <Text style={textStyle}>{text}</Text>
+>>>>>>> parent of 2307731 (review changes)
     </View>
   );
 }
