2307731513fe0b2626ac757932e3e8d7804ee3eb
AleGrz <ajemalbatros@gmail.com>
2026-05-04 19:38:16 +0200

    review changes

--- apps/mobile/src/components/core/Tag.component.tsx
+++ apps/mobile/src/components/core/Tag.component.tsx
@@ -1,41 +1,35 @@
-import { useMemo } from "react";
 import { Text, View } from "react-native";
 import { ColorPalette } from "@/styles/themes/theme";
-import { useUnistyles } from "react-native-unistyles";
+import { StyleSheet, useStyles } from "react-native-unistyles";
 
 type TagProps = {
   text: string;
   color?: ColorPalette;
 };
 
+const stylesheet = StyleSheet.create((theme) => ({
+  container: (color: ColorPalette) => ({
+    height: theme.size.lg,
+    borderRadius: theme.borderRadius.full,
+    backgroundColor: theme.colors[color].background.accent,
+    borderColor: theme.colors[color].main,
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: theme.spacing.sm,
+  }),
+  text: (color: ColorPalette) => ({
+    color: theme.colors[color].main,
+    fontSize: theme.size.sm,
+  }),
+}));
+
 export default function Tag({ text, color = "primary" }: TagProps) {
-  const { theme } = useUnistyles();
-
-  const containerStyle = useMemo(
-    () => ({
-      height: theme.size.lg,
-      borderRadius: theme.borderRadius.full,
-      backgroundColor: theme.colors[color].background.accent,
-      borderColor: theme.colors[color].main,
-      borderWidth: 1,
-      alignItems: "center" as const,
-      justifyContent: "center" as const,
-      paddingHorizontal: theme.spacing.sm,
-    }),
-    [theme.colors[color], theme.borderRadius.full, theme.size.sm, theme.spacing.xs],
-  );
-
-  const textStyle = useMemo(
-    () => ({
-        color: theme.colors[color].main,
-        fontSize: theme.size.sm,
-    }),
-    [theme.colors[color].main],
-  )
+  const { styles } = useStyles(stylesheet);
 
   return (
-    <View style={containerStyle}>
-      <Text style={textStyle}>{text}</Text>
+    <View style={styles.container(color)}>
+      <Text style={styles.text(color)}>{text}</Text>
     </View>
   );
 }
