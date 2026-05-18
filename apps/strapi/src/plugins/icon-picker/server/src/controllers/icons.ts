ff4a570e26b7f6a4954474e5bdc12cefa4fb311b
Jakub Jurczyk <pandetthe@gmail.com>
2026-05-18 19:07:30 +0200

    fix: missing typecheck for strapi

--- apps/strapi/src/plugins/icon-picker/server/src/controllers/icons.ts
+++ apps/strapi/src/plugins/icon-picker/server/src/controllers/icons.ts
@@ -1,8 +1,9 @@
 import type { Core } from "@strapi/strapi";
+import type { Context } from "koa";
 import { PLUGIN_ID } from "../pluginId";
 
 const icons = ({ strapi }: { strapi: Core.Strapi }) => ({
-  async list(ctx: any) {
+  async list(ctx: Context) {
     const iconNames = await strapi.plugin(PLUGIN_ID).service("icons").list();
     ctx.body = { icons: iconNames };
   },
